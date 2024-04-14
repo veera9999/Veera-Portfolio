c222a1eec7660b147f864d38e99b11a0979e6566
Veera Palla <veera.pa999@gmail.com>
2024-04-14 12:28:43 -0500

    Made Changes to Tech Stack

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -22,6 +22,24 @@ import {
 import { FaJava } from 'react-icons/fa';
 
 function Techstack() {
+  const toolNameMap = {
+    SiCplusplus: 'C++',
+    DiPython: 'Python',
+    FaJava: 'Java',
+    DiJavascript1: 'JavaScript',
+    DiNodejs: 'Node.js',
+    DiReact: 'React',
+    DiMongodb: 'MongoDB',
+    SiScikitlearn: 'Scikit-learn',
+    SiQiskit: 'Qiskit',
+    SiPytorch: 'PyTorch',
+    SiTensorflow: 'TensorFlow',
+    SiFirebase: 'Firebase',
+    DiSpark: 'Spark',
+    DiRust: 'Rust',
+    SiGnubash: 'Bash',
+  };
+
   return (
     <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
       {[
@@ -40,9 +58,12 @@ function Techstack() {
         <DiSpark />,
         <DiRust />,
         <SiGnubash />,
-      ].map((tool) => (
-        <Col xs={4} md={2} className="tech-icons">
+      ].map((tool, index) => (
+        <Col xs={4} md={2} className="tech-icons" key={index}>
           {tool}
+          <div className="tech-icon-hover-info">
+            <span>{toolNameMap[tool.type.name]}</span>
+          </div>
         </Col>
       ))}
     </Row>
