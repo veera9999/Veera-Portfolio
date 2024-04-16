fed617ff5bcc8d429a6370c0abd971e09a5112df
Veera Palla <veera.pa999@gmail.com>
2024-04-16 00:08:26 -0500

    Updated UI of Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,28 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
 import descriptions from '../description';
+import emailjs from '@emailjs/browser';
 function Contact() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_f11qcd8', 'template_6d5emjb', form.current, {
+        publicKey: 'tlt_veAa0s0pOSSaq',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <Container fluid className="contact-section" id="contact">
       <Container>
-        <h1 className="experience-heading">
+        <h1 className="contact-heading">
           {descriptions.ContactHeader}{' '}
           <strong className="green"> Touch </strong>
         </h1>
 
         <Row>
           <Col md={6}>
-            <Form>
+            <Form ref={form} onSubmit={sendEmail}>
               <Form.Group controlId="formName">
                 <Form.Control
+                  className="contact-form"
                   type="text"
                   placeholder="Your Name"
                   value={name}
+                  name="user_name"
                   onChange={(e) => setName(e.target.value)}
                   required
                 />
@@ -30,7 +52,9 @@ function Contact() {
 
               <Form.Group controlId="formEmail">
                 <Form.Control
+                  className="contact-form"
                   type="email"
+                  name="user_email"
                   placeholder="Your Email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
@@ -40,16 +64,23 @@ function Contact() {
 
               <Form.Group controlId="formMessage">
                 <Form.Control
+                  className="contact-form"
                   as="textarea"
                   rows={3}
                   placeholder="Message"
+                  name="message"
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
                   required
                 />
               </Form.Group>
 
-              <Button variant="primary" type="submit">
+              <Button
+                variant="primary"
+                type="submit"
+                value="Send"
+                style={{ padding: '10px' }}
+              >
                 Submit
               </Button>
             </Form>
