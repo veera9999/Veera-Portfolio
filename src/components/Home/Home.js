de59b48034717912ef22b3971bfdb97cae10d883
Veera Palla <veera.pa999@gmail.com>
2024-04-17 01:51:10 -0500

    Testing Github Actions

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -34,7 +34,7 @@ function Home() {
 
               <h1 className="heading-name">
                 {description.Im}
-                <strong className="main-name"> Veera</strong>
+                <strong className="main-name"> Veera Palla</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: 'left' }}>
