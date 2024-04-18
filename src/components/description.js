b3fa01d79968bba66896691d99b9293904ec9f00
Veera Palla <veera.pa999@gmail.com>
2024-04-18 03:28:47 -0500

    Made changes to project-card2

--- src/components/description.js
+++ src/components/description.js
@@ -34,7 +34,7 @@ const description = {
   Proj2:
     'This project involves in-depth data analysis on a Netflix dataset and developing a user recommendation model using PyTorch collaborative filtering techniques. The goal is to gain insights into user viewing patterns and preferences, and leverage this information to build a robust recommendation system.',
   Proj3:
-    'Python toolkit for quantum machine learning, developed on top of Qiskit, offers well-known supervised and unsupervised machine learning implementations for a gated quantum computer. Winner of a University award for best project.',
+    'This project aims to analyze the performance data of Skywest Airlines and compare it with other major airlines. It involves data collection, exploratory data analysis, benchmarking, and providing recommendations to Skywest management. The goal is to gain insights into Skywest operational efficiency, service quality, and overall performance, and identify areas for potential improvement.',
   Proj4:
     'Personal website portfolio built with React, NodeJS and ❤️. Used React-Bootstrap, Rect-Particles Libraries for styling. Used Github Actions for CI/CD and Github Pages for deployment.',
   Proj5:
