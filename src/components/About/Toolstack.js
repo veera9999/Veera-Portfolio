4ef50f301a4377b59891caa110a6ce5ff3520473
Veera Palla <veera.pa999@gmail.com>
2024-05-22 17:29:11 -0500

    Made Changes to tool stack

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -20,50 +20,32 @@ import {
 } from 'react-icons/si';
 import { BiLogoPostgresql } from 'react-icons/bi';
 function Toolstack() {
-  const toolNameMap = {
-    SiLinux: 'Linux',
-    SiGit: 'Git',
-    SiAmazonaws: 'Amazon AWS',
-    SiDocker: 'Docker',
-    SiJupyter: 'Jupyter',
-    SiGooglecolab: 'Google Colab',
-    SiApachetomcat: 'Apache Tomcat',
-    SiVercel: 'Vercel',
-    SiGithub: 'Github',
-    SiJira: 'Jira',
-    SiPostman: 'Postman',
-    SiSwagger: 'Swagger',
-    SiAmazonrds: 'Amazon RDS',
-    SiMongodb: 'MongoDB',
-    SiOracle: 'Oracle',
-    SiMysql: 'MySQL',
-    BiLogoPostgresql: 'postgreSQL',
-  };
+  const toolStack = [
+    { icon: <SiLinux />, name: 'Linux' },
+    { icon: <SiGit />, name: 'Git' },
+    { icon: <SiAmazonaws />, name: 'Amazon AWS' },
+    { icon: <SiGithub />, name: 'Github' },
+    { icon: <SiDocker />, name: 'Docker' },
+    { icon: <SiJupyter />, name: 'Jupyter' },
+    { icon: <SiGooglecolab />, name: 'Google Colab' },
+    { icon: <SiApachetomcat />, name: 'Apache Tomcat' },
+    { icon: <SiVercel />, name: 'Vercel' },
+    { icon: <SiJira />, name: 'Jira' },
+    { icon: <SiPostman />, name: 'Postman' },
+    { icon: <SiSwagger />, name: 'Swagger' },
+    { icon: <SiAmazonrds />, name: 'Amazon RDS' },
+    { icon: <SiMongodb />, name: 'MongoDB' },
+    { icon: <SiOracle />, name: 'Oracle' },
+    { icon: <BiLogoPostgresql />, name: 'PostgreSQL' },
+    { icon: <SiMysql />, name: 'MySQL' },
+  ];
   return (
     <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
-      {[
-        <SiLinux />,
-        <SiGit />,
-        <SiAmazonaws />,
-        <SiGithub />,
-        <SiDocker />,
-        <SiJupyter />,
-        <SiGooglecolab />,
-        <SiApachetomcat />,
-        <SiVercel />,
-        <SiJira />,
-        <SiPostman />,
-        <SiSwagger />,
-        <SiAmazonrds />,
-        <SiMongodb />,
-        <SiOracle />,
-        <BiLogoPostgresql />,
-        <SiMysql />,
-      ].map((tool, index) => (
+      {toolStack.map((tool, index) => (
         <Col xs={4} md={2} className="tech-icons" key={index}>
-          {tool}
+          <div className="icon-images">{tool.icon}</div>
           <div className="tech-icon-hover-info">
-            <span>{toolNameMap[tool.type.name]}</span>
+            <span>{tool.name}</span>
           </div>
         </Col>
       ))}
